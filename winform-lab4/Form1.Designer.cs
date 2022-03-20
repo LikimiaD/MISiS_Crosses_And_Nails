3ba307b98477d4b1bd0f249caa84001b56c6b6ab
LikimiaD <somethingforsoftware@gmail.co>
2022-03-20 20:11:37 +0300

    20/03/2022

--- winform-lab4/Form1.Designer.cs
+++ winform-lab4/Form1.Designer.cs
@@ -29,23 +29,61 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.playertext = new System.Windows.Forms.RichTextBox();
+            this.player1 = new System.Windows.Forms.RichTextBox();
+            this.player2 = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(14, 16);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(462, 488);
+            this.panel1.Size = new System.Drawing.Size(528, 651);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
             // 
+            // playertext
+            // 
+            this.playertext.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.playertext.Font = new System.Drawing.Font("Segoe UI", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.playertext.Location = new System.Drawing.Point(199, 690);
+            this.playertext.Name = "playertext";
+            this.playertext.Size = new System.Drawing.Size(174, 58);
+            this.playertext.TabIndex = 1;
+            this.playertext.Text = "";
+            // 
+            // player1
+            // 
+            this.player1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.player1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player1.Location = new System.Drawing.Point(14, 690);
+            this.player1.Name = "player1";
+            this.player1.Size = new System.Drawing.Size(141, 58);
+            this.player1.TabIndex = 2;
+            this.player1.Text = "";
+            // 
+            // player2
+            // 
+            this.player2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.player2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.player2.Location = new System.Drawing.Point(412, 690);
+            this.player2.Name = "player2";
+            this.player2.Size = new System.Drawing.Size(141, 58);
+            this.player2.TabIndex = 3;
+            this.player2.Text = "";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 513);
+            this.ClientSize = new System.Drawing.Size(565, 779);
+            this.Controls.Add(this.player2);
+            this.Controls.Add(this.player1);
+            this.Controls.Add(this.playertext);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -55,5 +93,8 @@
         #endregion
 
         private Panel panel1;
+        private RichTextBox playertext;
+        private RichTextBox player1;
+        private RichTextBox player2;
     }
 }
--- winform-lab4/Form1.cs
+++ winform-lab4/Form1.cs
@@ -2,62 +2,130 @@
 {
     public partial class Form1 : Form
     {
-        public int polesize = 10;
+        public int polesize = 3;
         public int bordervert = 300, borderhoriz = 300; 
         public int player = 1;
         public int cellsize;
         public bool game = true;
+        public int gamecount = 1;
+        public int playerf = 0, players = 0;
         public Dictionary<string, string> arr = new Dictionary<string, string>();
-        public Form1() => InitializeComponent();
-         /*Чилловая сетка
-         * Строит polesize на polesize */
-        private void panel1_Paint(object sender, PaintEventArgs e){
+        public Form1()
+        {
+            InitializeComponent();
+            create_dic(true);
+            playertext.Text = "Игрок 1";
+        }
+            /*Чилловая сетка
+            * Строит polesize на polesize */
+            private void panel1_Paint(object sender, PaintEventArgs e){
             int i = 0;
             int x1 = 0, x2 = 0;
             int yy1 = 0, yy2 = 0;
             cellsize = bordervert / polesize;
-            create_dic();
-            while (i != polesize + 1){
+            while (i != polesize + 1)
+            {
                 e.Graphics.DrawLine(new Pen(Color.Black, 3), x1, 0, x2, borderhoriz);
                 e.Graphics.DrawLine(new Pen(Color.Black, 3), 0, yy1, bordervert, yy2);
                 x1 += cellsize; x2 += cellsize;
                 yy1 += cellsize; yy2 += cellsize;
-                i++;}}
+                i++;
+            }
+            for (int x = 0; x < polesize; x++)for (int g = 0; g < polesize; g++)
+                {
+                    if (arr[$"{x}-{g}"] == "X")draw_сross(1, x, g);
+                    else if (arr[$"{x}-{g}"] == "O")draw_ellipse(1, x, g);
+                }
+            player1.Text = $"Игрок 1\tСчет: {playerf}";
+            player2.Text = $"Игрок 2\tСчет: {players}";
+            }
         /*А че будет если кликнуть?*/
         private void panel1_MouseClick(object sender, MouseEventArgs e){
             /* 0 -> Player 1
              * 1 -> Player 2*/
             if (game){
                 if ((e.X < borderhoriz) && (e.Y < bordervert)){
-                    player = player == 0 ? 1 : 0;
                     if (e.Button == MouseButtons.Left){
-                        if (player == 0) draw_ellipse(e.X, e.Y);
-                        else draw_сross(e.X, e.Y);}
+                        if (player == 0) draw_ellipse(0, e.X, e.Y);
+                        else draw_сross(0, e.X, e.Y);}
                     if(win() == 1){
-                        MessageBox.Show("You win");
-                        game = false;
-                    };}}}
+                        MessageBox.Show($"Игрок {(player == 0 ? 1 : 2)} выйграл");
+                        if (player == 0) playerf += 1;
+                        else players += 1;
+                        player = 1;
+                        create_dic(false);
+                        panel1.Refresh();
+                    }
+                else if (gamecount == Math.Pow(polesize, 2))
+                        {
+                        MessageBox.Show($"Ничья");
+                        player = 1;
+                        create_dic(false);
+                        panel1.Refresh();
+                        }
+                }}}
         /*Кружок*/
-        private void draw_ellipse(int MouseX, int MouseY){
+        private void draw_ellipse(int kekw, int MouseX, int MouseY)
+        {
             /*А вдруг уже кто-то тут закрасил.а-а-а?*/
-            if (arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] == arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"]){
+            if (kekw == 0)
+            {
+                if (arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] == $"{okr(1, MouseX)}-{okr(1, MouseY)}")
+                {
+                    gamecount += 1;
+                    player = player == 0 ? 1 : 0;
+                    playertext.Text = $"Игрок {(player == 0 ? 2 : 1)}";
+                    Graphics g = panel1.CreateGraphics();
+                    g.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(new Point(okr(0, MouseX), okr(0, MouseY)), new Size(cellsize, cellsize)));
+                    arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] = "O";
+                    g.Dispose();
+                }
+            }
+            else
+            {
                 Graphics g = panel1.CreateGraphics();
-                g.DrawEllipse(new Pen(Color.Red, 3),new Rectangle(new Point(okr(0, MouseX), okr(0, MouseY)),new Size(cellsize, cellsize)));
-                arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] = "O";
-                g.Dispose();}}
+                g.DrawEllipse(new Pen(Color.Red, 3), new Rectangle(new Point(MouseX * cellsize, MouseY * cellsize), new Size(cellsize, cellsize)));
+                g.Dispose();
+            }
+        }
         /*Крестик*/
-        private void draw_сross(int MouseX, int MouseY){
+        private void draw_сross(int kekw, int MouseX, int MouseY)
+        {
             /*А вдруг уже кто-то тут закрасил.а-а-а?*/
-            if (arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] == arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"]){
+            if (kekw == 0)
+            {
+                if (arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] == $"{okr(1, MouseX)}-{okr(1, MouseY)}")
+                {
+                    gamecount += 1;
+                    player = player == 0 ? 1 : 0;
+                    playertext.Text = $"Игрок {(player == 0 ? 2 : 1)}";
+                    Graphics g = panel1.CreateGraphics();
+                    g.DrawLine(new Pen(Color.Blue, 3), new Point(okr(0, MouseX), okr(0, MouseY)), new Point(okr(0, MouseX) + cellsize, okr(0, MouseY) + cellsize));
+                    g.DrawLine(new Pen(Color.Blue, 3), new Point(okr(0, MouseX) + cellsize, okr(0, MouseY)), new Point(okr(0, MouseX), okr(0, MouseY) + cellsize));
+                    arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] = "X";
+                    g.Dispose();
+                }
+            }
+            else
+            {
                 Graphics g = panel1.CreateGraphics();
-                g.DrawLine(new Pen(Color.Blue, 3),new Point(okr(0, MouseX), okr(0, MouseY)),new Point(okr(0, MouseX) + cellsize, okr(0, MouseY) + cellsize));
-                g.DrawLine(new Pen(Color.Blue, 3),new Point(okr(0, MouseX) + cellsize, okr(0, MouseY)),new Point(okr(0, MouseX), okr(0, MouseY) + cellsize));
-                arr[$"{okr(1, MouseX)}-{okr(1, MouseY)}"] = "X";
-                g.Dispose(); }}
+                g.DrawLine(new Pen(Color.Blue, 3), new Point(MouseX * cellsize, MouseY * cellsize), new Point(MouseX * cellsize + cellsize, MouseY * cellsize + cellsize));
+                g.DrawLine(new Pen(Color.Blue, 3), new Point(MouseX * cellsize, MouseY * cellsize), new Point(MouseX * cellsize, MouseY * cellsize + cellsize));
+                g.Dispose();
+            }
+        }
         /*Округление в меньшую сторону*/
         private int okr(int value, int x) => value == 0 ? (int)Math.Floor((decimal) x / cellsize) * cellsize : (int)Math.Floor((decimal)x / cellsize);
         /*Cоздание словарика для записи ответиков*/
-        private void create_dic() {for (int i = 0; i < polesize; i++) for (int g = 0; g < polesize; g++)arr.Add($"{i}-{g}", $"{i}-{g}");}
+        private void create_dic(bool kekw)
+        {
+            for (int i = 0; i < polesize; i++) for (int g = 0; g < polesize; g++)
+                {
+                    if (kekw) arr.Add($"{i}-{g}", $"{i}-{g}");
+                    else arr[$"{i}-{g}"] = $"{i}-{g}";
+                }
+        }
+
         private int win(){
             var num = 0;
             if (polesize == 3){
